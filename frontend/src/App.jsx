85347dd190a658d472f76209c8b7c6cd373d9cd3
hasharas <hasharamsankalpam@gmail.com>
2024-05-05 00:33:22 +0530

    route the home page,card,place order .setup this things

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Card from './pages/Card/Card.jsx';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        < Route path='/' element = {<Home/>} />
+        < Route path='/card' element = {<Card/>} />
+        < Route path='/placeorder' element = {<PlaceOrder/>} />
+
+      </Routes>
     </div>
   );
 }
