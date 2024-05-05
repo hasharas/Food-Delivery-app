a3b4122735b185cc3a754c0d3a62f680ccece9da
hasharas <hasharamsankalpam@gmail.com>
2024-05-05 13:22:19 +0530

    create menu item section and style it

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
        <div className="header-content">
             <h2>Order your favorite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes creafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-            <button>View Menu</button>
+            <button className='button'>View Menu</button>
        </div>
     </div>
   );
