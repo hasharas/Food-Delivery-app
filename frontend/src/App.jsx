a2e35053c0fa36da1a666069f9d87be8331f8bad
hasharas <hasharamsankalpam@gmail.com>
2024-05-07 00:16:47 +0530

    create footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,23 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Card from './pages/Card/Card.jsx';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 const App = () => {
   return (
+  <>
     <div className='app'>
-      <Navbar />
-      <Routes>
-        < Route path='/' element = {<Home/>} />
-        < Route path='/card' element = {<Card/>} />
-        < Route path='/placeorder' element = {<PlaceOrder/>} />
+        <Navbar />
+        <Routes>
 
-      </Routes>
+            < Route path='/' element = {<Home/>} />
+            < Route path='/card' element = {<Card/>} />
+            < Route path='/placeorder' element = {<PlaceOrder/>} />
+
+        </Routes>
     </div>
+        <Footer />
+  </>
   );
 }
 
