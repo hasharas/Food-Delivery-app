1f9cb811f79539609cdcc405f6f71de5eb647354
hasharas <hasharamsankalpam@gmail.com>
2024-05-07 03:13:18 +0530

    createing sign up component

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import './LoginPopup.css';
+import { assets } from '../../assets/assets';
+
+const LoginPopup = ({setShowLogin}) => {
+
+      const [currState, setCurrState] = useState("Sign Up");
+
+  return (
+    <div className='login-popup'>
+       <form className='login-popup-container'>
+            <div className="login-popup-title">
+                  <h2>{}currState</h2>
+                  <img onClick={() => setShowLogin(false) } src={assets.cross_icon} alt="" />
+            </div>
+       </form>
+    </div>
+  );
+}
+
+export default LoginPopup;
