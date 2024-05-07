3acee58aeb4a9a337bdfe8ad0343041a640aacd1
hasharas <hasharamsankalpam@gmail.com>
2024-05-07 23:53:11 +0530

    Create card pay page

--- frontend/src/pages/Card/Card.jsx
+++ frontend/src/pages/Card/Card.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import "./Card.css";
+
+import "./Card.css";  
+import React, { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+
 
 const Card = () => {
+
+    const { CardItems, food_list, removeFromCard } = useContext(StoreContext);
+
   return (
-    <div>
-      
+    <div className='card'>
+          <div className="card-items">
+                <div className="card-items-title">
+                      <p>Items</p>
+                      <p>Title</p>
+                      <p>Price</p>
+                      <p>Quantity</p>
+                      <p>Totle</p>
+                      <p>Remove</p>
+                </div> 
+                <br />
+                <hr /> 
+          <div>
+                  {food_list.map((item,index) => {
+                    if(CardItems[item._id]>0){
+                      return (
+                        <div key={item._id} className="card-items-title card-items-item">
+                            <img src={item.image} alt="" />
+                            <p>{item.name}</p>
+                            <p>{item.price}</p>
+                            <p>{CardItems[item._id]}</p>
+                            <p>{item.price*CardItems[item._id]}</p>
+                            <p>x</p>
+                        </div>
+                      )
+                    }
+                  }
+                )
+                  }
+               </div>
+          </div>      
     </div>
   );
 }
