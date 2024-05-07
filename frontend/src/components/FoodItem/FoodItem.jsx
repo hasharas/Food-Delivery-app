fcddbe5c26f33c7ec5098926a4b20bad1068b089
hasharas <hasharamsankalpam@gmail.com>
2024-05-08 00:53:39 +0530

    style card pay page and add more function card pay page

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,41 +1,51 @@
-import React, {useContext} from 'react';
-import './FoodItem.css';
-import { assets } from '../../assets/assets';
-import { StoreContext } from '../../context/StoreContext';
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
-const FoodItem = ({id,name,price,description,image}) => {
-
-    const {CardItems,addToCard,removeFromCard} = useContext(StoreContext);
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { CardItems, addToCard, removeFromCard } = useContext(StoreContext);
 
   return (
-    <div className='food-item'>
-            <div className="food-item-image-container">
-                  <img src={image} alt="" className="food-item-image" />
+    <div className="food-item">
+      <div className="food-item-image-container">
+        <img src={image} alt="" className="food-item-image" />
 
-                  {!CardItems[id]
-                              ?<img className='add' onClick={() =>addToCard(id) } src={assets.add_icon_white} alt=""/>
-                              :<div className="food-item-counter">
+        {!CardItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCard(id)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCard(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
 
-                                      <img onClick={() => removeFromCard(id) } src={assets.remove_icon_red} alt="" />
+            <p>{CardItems[id]}</p>
 
-                                      <p>{CardItems[id]}</p>
-
-                                      <img onClick={() => addToCard(id)} src={assets.add_icon_green} alt="" />
-
-                              </div>
-
-                  }
-            </div>
-            <div className="food-item-info">
-                <div className="food-item-name-rating">
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt="" />
-                </div>
-                <p className="food-item-desc">{description}</p>
-                <p className="food-item-price">${price}</p>
-            </div>
+            <img
+              onClick={() => addToCard(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default FoodItem;
--- frontend/src/pages/Card/Card.jsx
+++ frontend/src/pages/Card/Card.jsx
@@ -1,47 +1,50 @@
-
-import "./Card.css";  
-import React, { useContext } from 'react';
-import { StoreContext } from '../../context/StoreContext';
-
+import "./Card.css";
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
 const Card = () => {
-
-    const { CardItems, food_list, removeFromCard } = useContext(StoreContext);
+  const { CardItems, food_list, removeFromCard } = useContext(StoreContext);
 
   return (
-    <div className='card'>
-          <div className="card-items">
-                <div className="card-items-title">
-                      <p>Items</p>
-                      <p>Title</p>
-                      <p>Price</p>
-                      <p>Quantity</p>
-                      <p>Totle</p>
-                      <p>Remove</p>
-                </div> 
-                <br />
-                <hr /> 
-          <div>
-                  {food_list.map((item,index) => {
-                    if(CardItems[item._id]>0){
-                      return (
-                        <div key={item._id} className="card-items-title card-items-item">
-                            <img src={item.image} alt="" />
-                            <p>{item.name}</p>
-                            <p>{item.price}</p>
-                            <p>{CardItems[item._id]}</p>
-                            <p>{item.price*CardItems[item._id]}</p>
-                            <p>x</p>
-                        </div>
-                      )
-                    }
-                  }
-                )
-                  }
-               </div>
-          </div>      
+    <div className="card">
+      <div className="card-items">
+        <div className="card-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Totle</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        <div>
+          {food_list.map((item, index) => {
+            if (CardItems[item._id] > 0) {
+              return (
+                <div>
+                  <div className="card-items-title card-items-item">
+                    <img src={item.image} alt="" />
+                    <p>{item.name}</p>
+                    <p>${item.price}</p>
+                    <p>{CardItems[item._id]}</p>
+                    <p>${item.price * CardItems[item._id]}</p>
+                    <p
+                      onClick={() => removeFromCard(item._id)}
+                      className="cross"
+                    >
+                      x
+                    </p>
+                  </div>
+                  <hr />
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Card;
