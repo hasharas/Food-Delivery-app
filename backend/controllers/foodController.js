bdd0e38011d77965feeb2b3268229fa925d0fb65
hasharas <hasharamsankalpam@gmail.com>
2024-05-24 17:15:08 +0530

    create food controller js file

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,10 @@
+import foodModel from "../models/foodModel.js";
+import fc from "fs"
+
+//add food item
+
+const addFood = async (req,res) => {
+
+} 
+
+export {addFood}
--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -11,3 +11,4 @@ const foodSchema = new mongoose.Schema({
 const foodModel = mongoose.models.food || mongoose.model("food",foodSchema);
 
 export default foodModel;
+
