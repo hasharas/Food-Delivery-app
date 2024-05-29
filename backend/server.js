feb70d215ba733c907fb10d7936997f6b795b7cb
hasharas <hasharamsankalpam@gmail.com>
2024-05-29 22:25:56 +0530

    add foodrouter file to neaded things

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 //app config 
 
@@ -13,6 +14,9 @@ app.use(cors())
 
 // db connection 
 connectDB();
+
+//api endpoints
+app.use("/api/food",foodRouter)
 
 app.get("/",(req,res)=>{
       res.send("API Working")
