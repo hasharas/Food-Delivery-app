c28f75b52ba84e1444cc6979d6bc98318cd5d30e
hasharas <hasharamsankalpam@gmail.com>
2025-01-21 22:18:00 +0530

    connect:fix some db connecting problums

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-      await mongoose.connect('mongodb+srv://hasharamsankalpam:1111@cluster0.yqsksrw.mongodb.net/Food Delivery').then(()=>console.log("DB connected"));      
+      await mongoose.connect('mongodb+srv://food:food123@cluster0.n8duc.mongodb.net//Food-Delivery').then(() => console.log("DB connected successfully...."));
 }
+
+//user-food
+//pass-food123
+//mongodb+srv://food:food123@cluster0.n8duc.mongodb.net/?
