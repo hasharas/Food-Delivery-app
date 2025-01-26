22b02100536d231b78ff4f209d6619d5a604e3e7
hasharas <hasharamsankalpam@gmail.com>
2025-01-26 18:53:32 +0530

    Add:brouser router add

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
+import { BrouserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrouserRouter>
     <App />
-  </StrictMode>,
+
+  </BrouserRouter>
+
 )
