2b141edc23809ba360f1ff1c716a5114983dae01
hasharas <hasharamsankalpam@gmail.com>
2025-01-26 19:10:23 +0530

    Add:profile and logo add nav bar to

--- admin/src/component/Navbar/Navbar.jsx
+++ admin/src/component/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Navbar.css';
+import { assets } from '../../assets/assets.js';
 
 const Navbar = () => {
       return (
-            <div>
-
+            <div className='navbar'>
+                  <img className='logo' src={assets.logo} alt="logo" />
+                  <img className='profile' src={assets.profile_image} alt="profile image" />
             </div>
       );
 }
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.jsx';
-import { BrouserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <BrouserRouter>
+  <BrowserRouter>
     <App />
-
-  </BrouserRouter>
+  </BrowserRouter>
 
 )
