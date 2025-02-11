053c22e8e7e81acbb5e956de4a1b8046a16a0b0e
hasharas <hasharamsankalpam@gmail.com>
2025-02-11 19:56:44 +0530

    Add:create neccery things add page to

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import './Add.css'
+import { assets } from './../../assets/assets.js'
 
 const Add = () => {
       return (
-            <div>
+            <div className='add'>
+                  <form className="flex-col">
+                        <div className="add-image-upload flex col">
+                              <p>Upload Image</p>
+                              <label htmlFor="image">
+                                    <img src={assets.upload_area} alt="upload area" />
+                              </label>
+                              <input type="file" id='image' hidden required className='' />
+                        </div>
+                        <div className="add-product-name flex col">
+                              <p>Product Name</p>
+                              <input type="text" name='name' placeholder='Type hare..' />
+                        </div>
+                        <div className="add-product-description flex-col">
+                              <p>Description</p>
+                              <textarea name="description" rows='6' placeholder='Write content hare..' required id=""></textarea>
+                        </div>
+                        <div className="add-category-price">
+                              <div className="add-category flex-col">
+                                    <p>Product Category</p>
+                              </div>
+                        </div>
+                  </form>
 
             </div>
       );
