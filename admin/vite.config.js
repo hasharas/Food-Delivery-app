0973445608561ce9325b5b8027359d681ee8ce9e
hasharas <hasharamsankalpam@gmail.com>
2025-04-03 13:41:32 +0530

    Fix:port err fix

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Change to a different port
+    host: true, // Allow external access if needed
+  },
 })
